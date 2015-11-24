fdd6123da1877eee07d641e6f8f8642cc61c39c4
CHOQUET Simon <choquet.simon@gmail.com>
2015-11-24 16:11:08 +0100

    création des fichiers header, footer, sidebar, css et js

---
+++ wp-content/themes/chronostheme/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <title><?php bloginfo('name'); ?></title>
+
+        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
+
+        <script src="<?php bloginfo('template_url'); ?>/js/jquery-1.11.3.min.js"></script>
+        <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+
--- wp-content/themes/chronostheme/index.php
+++ wp-content/themes/chronostheme/index.php
@@ -1,1 +1,8 @@
 <?php
+    get_header();
+    get_sidebar();
+?>
+    <h1>Hello World!!</h1>
+<?php
+    get_footer();
+?>
