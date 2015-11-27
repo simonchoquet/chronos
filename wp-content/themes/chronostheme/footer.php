3ecc5c266a5e2cd224914157b876496d8c2410d6
CHOQUET Simon <choquet.simon@gmail.com>
2015-11-27 16:56:08 +0100

    ajout des mentions légales

--- wp-content/themes/chronostheme/footer.php
+++ wp-content/themes/chronostheme/footer.php
@@ -1,7 +1,10 @@
         </div>
+
+        <div class="text-center"><a href="<?php bloginfo('siteurl'); ?>/?page_id=19">Mentions Légales</a></div>
+
     </body>
+    <?php
+        wp_footer();
+    ?>
 </html>
 
-<?php
-    wp_footer();
-?>
---
+++ wp-content/themes/chronostheme/mentionslegales.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Mentions Légales
+ */
+get_header();
+?>
+<h2>Mentions Légales</h2>
+<?php
+the_post();
+the_content();
+
+get_footer();
